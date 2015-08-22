1e45abcee328fe104760916e9fb9853c71cd47b9
xcoole <hwclegend@163.com>
2015-08-22 12:36:37 +0800

    群集优化

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 //全局配置
-var cosjs  = require('./lib/cosjs');
 var config = require('./config');
-var dataset = new cosjs.dataset(config);
+var library  = require('./lib/library');
+var dataset = new library.dataset(config);
 
-exports.set     = dataset.set;
-exports.lib     = cosjs;
-exports.http    = require('./lib/http').create;
+exports.set      = dataset.set;
+exports.http     = require('./lib/http').create;
 exports.config   = config;
 exports.cluster = require('./lib/cluster');
-
+exports.library = library;
