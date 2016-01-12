261bda43126789ba55ddcb4001f99423607cbde5
xcoole <hwclegend@163.com>
2016-01-12 15:02:11 +0800

    bug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var cluster = require('./lib/cluster');
 exports.fork = cluster.fork;
 exports.start = cluster.start;
 
-['http','pool','task','dataset','redis','mongodb','session'].forEach(function(k){
+['http','pool','task','dataset','redis','mongodb','session','library'].forEach(function(k){
     exports[k] = require('./lib/'+k);
 });
 
