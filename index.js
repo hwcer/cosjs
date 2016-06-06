17354cbf8a92fb0bc15af400b80a6124f0d2519f
xcoole <hwclegend@163.com>
2016-06-06 16:36:20 +0800

    update

--- index.js
+++ index.js
@@ -3,24 +3,7 @@ var library = require('./lib/library');
 exports = module.exports = require('./lib/cluster');
 
 //HTTP SERVER
-exports.http = function(key,work,port,handle){
-    var http = require('./lib/http');
-    if(!work){
-        work = require('os').cpus().length;
-    }
-    exports.fork( key||'http', work , function(){
-        http(port,handle)
-    });
-}
-
-
-//SOCKET SERVER
-exports.socket = function(key,work,port,handle){
-    var socket = require('./lib/socket');
-    exports.fork( key||'socket', 1 , function(){
-        socket(port,handle)}
-    );
-}
+exports.http = require('./lib/http');
 
 
 new Array("pool","session").forEach(function (name) {
