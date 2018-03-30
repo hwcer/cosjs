814873cc56b2168f1086e522e0d5d1cc82d7e09e
xcoole <hwclegend@163.com>
2018-03-30 18:17:23 +0800

    update

--- index.js
+++ index.js
@@ -27,7 +27,17 @@ exports.https = function(opts){
 
 
 
-
+if (!Object.values) Object.values = function(obj) {
+    if (obj !== Object(obj))
+        throw new TypeError('Object.values called on a non-object');
+    var val=[],key;
+    for (key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj,key)) {
+            val.push(obj[key]);
+        }
+    }
+    return val;
+}
 
 
 
